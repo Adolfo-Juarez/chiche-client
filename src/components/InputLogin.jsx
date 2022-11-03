7368e452eaaefdee65217653d0f9e2220e4bdf9e
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2022-11-02 22:11:40 -0600

    Login and register 50 porcent finished

---
+++ src/components/InputLogin.jsx
@@ -0,0 +1,20 @@
+import '../assets/stylesheets/login.css'
+
+function InputLogin() {
+    return (  
+    <div className="img-login">
+        
+    <div className='form-login'>
+    <h2 className='text-login'>BIENVENIDO</h2>
+    <input type="text"placeholder="Usuario o correo electronico"/>
+    <input type="password" placeholder="Contrasena"/>
+    <button className="btn-login">Inicia sesion</button>
+    </div>
+    <p>No tienes una cuenta?</p>
+    <a href="">Registrate aqui</a>
+    </div>
+    
+    );
+}
+
+export default InputLogin;
