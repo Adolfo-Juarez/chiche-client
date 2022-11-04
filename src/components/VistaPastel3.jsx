1ff47aab4150a6e7aa8d91459c8e7a25d8633403
Diego Carmona Bernal <cbdiegox@gmail.com>
2022-11-04 00:29:28 -0600

    update

--- src/components/VistaPastel3.jsx
+++ src/components/VistaPastel3.jsx
@@ -1,12 +1,12 @@
-import "../assets/stylesheets/MainVistaPastel3.css";
-import c1 from '../assets/icons/C1.svg'
-import c2 from '../assets/icons/C2.svg'
-import c3 from '../assets/icons/C3.svg'
-import c4 from '../assets/icons/C4.svg'
-import c5 from '../assets/icons/C5.svg'
-import c6 from '../assets/icons/C6.svg'
+import "../assets/stylesheets/VistaPastel.css";
+import c1 from "../assets/icons/C1.svg";
+import c2 from "../assets/icons/C2.svg";
+import c3 from "../assets/icons/C3.svg";
+import c4 from "../assets/icons/C4.svg";
+import c5 from "../assets/icons/C5.svg";
+import c6 from "../assets/icons/C6.svg";
 
-function MainVistaPastel3() {
+function VistaPastel3() {
   return (
     <>
       <div className="sub1">
@@ -60,4 +60,4 @@ function MainVistaPastel3() {
   );
 }
 
-export default MainVistaPastel3;
+export default VistaPastel3;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./views/OrderPastel3";
+import PagePastel1 from "./views/OrderPastel2";
+import PagePastel2 from "./views/OrderPastel2";
+import PagePastel3 from "./views/OrderPastel3";
 import "./assets/stylesheets/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <PagePastel2 />
   </React.StrictMode>
 );
