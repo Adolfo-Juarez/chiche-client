333286c763a824b6c06b8fd7470e443dd300eea5
Diego Carmona Bernal <cbdiegox@gmail.com>
2022-11-03 22:14:42 -0600

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,14 @@ import imgPedidos from "../assets/img/archive.png";
 import imgAyuda from "../assets/img/question.png";
 import "../assets/stylesheets/Header.css";
 
+const toggle = document.querySelector(".btn-nav");
+const links = document.querySelector(".links");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("rotate");
+  links.classList.toggle("active");
+});
+
 function Header() {
   return (
     <>
@@ -13,7 +21,7 @@ function Header() {
         <img className="logo" src={logo} alt="Logo header" />
         <a className="btn-nav" href="#"></a>
         <ul className="links">
-          <img src={logoChico} alt="Logo nav" />
+          <img src={logoChico} alt="Logo nav" width="60px"/>
           <li className="link">
             <a href="#">Inicio</a>
           </li>
