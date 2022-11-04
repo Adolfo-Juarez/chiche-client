5507fb51c280ba6e18e2e8e154ba29557a210bab
Diego Carmona Bernal <cbdiegox@gmail.com>
2022-11-04 00:02:43 -0600

    Footer

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,15 +1,17 @@
 import Header from "../components/Header";
 import Barra from "../components/Barra";
 import MainVistaPastel3 from "../components/MainVistaPastel3";
+import Footer from "../components/Footer";
 
-function Test() {
+function Home() {
   return (
     <>
       <Header></Header>
       <Barra></Barra>
       <MainVistaPastel3></MainVistaPastel3>
+      <Footer></Footer>
     </>
   );
 }
 
-export default Test;
+export default Home;
