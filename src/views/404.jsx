c6f2af0312fbe49fb54883bd03478813c95faefb
Adolfo-Juarez <213358@ids.upchiapas.edu.mx>
2022-11-06 15:30:53 -0600

    feat: 404 error page added

--- src/views/404.jsx
+++ src/views/404.jsx
@@ -1,11 +1,19 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import '../assets/stylesheets/404.css'
+import oDonn from '../assets/img/bittenDonnut.jpg'
 
 function E404() {
     return ( 
         <>
         <Header/>
-        <h1>No hemos podido encontrar la página que buscas:(;</h1>
+        <main>
+            <h1 className="not-found-header">4<img src={oDonn} style={{width:130}}></img>4</h1>
+            <h2 className="not-found-caption"> Ups! No hemos podido encontrar la ruta solicitada</h2>
+            <p  className="not-found-back">
+            <a href="/">Volver a la página principal</a>
+            </p>
+        </main>
         <Footer/>
         </>
      );
