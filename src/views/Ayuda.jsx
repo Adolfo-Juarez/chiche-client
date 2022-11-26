4fb0b6b9ac3c608a3d90d64012aa7b6c8bd2d33b
Diego Carmona Bernal <cbdiegox@gmail.com>
2022-11-25 19:50:30 -0600

    fixes views

--- src/views/Ayuda.jsx
+++ src/views/Ayuda.jsx
@@ -7,33 +7,35 @@ function Help() {
     <>
       <Header />
       <section className="help">
-        <h1 className="title-help">Ayuda</h1>
-        <p className="content-help">
-          Bienvenido a la sección de ayuda de Chiché.com
-        </p>
-        <h2 className="second-title">¿Algún error durante su navegación?</h2>
-        <p className="content-help">
-          Reporta cualquier Bug en nuestro repositorio de{" "}
-          <a
-            href="https://github.com/Adolfo-Juarez/chiche-client/issues"
-            target="_blank"
-          >
-            Github.
-          </a>
-        </p>
-        <h2 className="second-title">Contribuye al proyecto</h2>
-        <p className="content-help">
-          Puedes hacer{" "}
-          <a
-            href="https://github.com/Adolfo-Juarez/chiche-client/pulls"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            pull request
-          </a>{" "}
-          a nuestro proyecto en cualquier momento
-        </p>
-        <p className="second-title">Version 1.0.0</p>
+        <div className="content">
+          <h1 className="title-help">Ayuda</h1>
+          <p className="content-help">
+            Bienvenido a la sección de ayuda de Chiché.com
+          </p>
+          <h2 className="second-title">¿Algún error durante su navegación?</h2>
+          <p className="content-help">
+            Reporta cualquier Bug en nuestro repositorio de{" "}
+            <a
+              href="https://github.com/Adolfo-Juarez/chiche-client/issues"
+              target="_blank"
+            >
+              Github.
+            </a>
+          </p>
+          <h2 className="second-title">Contribuye al proyecto</h2>
+          <p className="content-help">
+            Puedes hacer{" "}
+            <a
+              href="https://github.com/Adolfo-Juarez/chiche-client/pulls"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              pull request
+            </a>{" "}
+            a nuestro proyecto en cualquier momento
+          </p>
+          <p className="second-title">Version 1.0.0</p>
+        </div>
       </section>
       <Footer />
     </>
