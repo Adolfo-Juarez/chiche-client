70527d4de8a4307d80994b2da586e446bcfa99e6
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2022-12-06 14:39:23 -0600

    Login already finished

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,76 +3,61 @@ import { useEffect, useState } from "react";
 import { authenticate, isAuthenticated } from "../resources/Auth";
 import { useNavigate } from "react-router-dom";
 
-
 function LoginForm() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
 
-    const navigate = useNavigate();
-    const [email, setEmail] = useState(null);
-    const [password, setPassword] = useState(null);
+  function login(e) {
+    e.preventDefault();
+    authenticate(email, password).then((data) => {
+      if (!data.logged) {
+        alert("Verifique sus datos");
+      } else {
+        navigate("/");
+      }
+    });
+  }
 
-    function login(e){
-        e.preventDefault()
-        authenticate(email,password).then(
-            (data) => {
-                if (!data.logged) {
-                  alert("Verifique sus datos")
-                } else {
-                  navigate("/")
-                }
-            }
-        )
+  useEffect(() => {
+    if (isAuthenticated()) {
+      alert("Ya tienes una sesión iniciada");
+      navigate("/");
     }
+  }, []);
 
-    useEffect(() => { if(isAuthenticated()){
-        alert("Ya tienes una sesión iniciada")
-        navigate("/")
-      } }, [])
-
-    return (
-        <form className="register-register" onSubmit={login}>
-            <h2 className="register-title">Iniciar sesión</h2>
-            <label className="labels-register" htmlFor="email">
-                Nombre de usuario
-            </label>
-            <input
-                type="text"
-                id="email"
-                className="input-register"
-                onChange={(e) => {
-                    setEmail(e.target.value);
-                }}
-            />
-
-            <label className="labels-register" htmlFor="password">
-                Contraseña &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </label>
-            <input
-                type="password"
-                id="password"
-                className="input-register"
-                onChange={(e) => setPassword(e.target.value)}
-            />
-
-            <input type="submit" className="register-btn" value="Ingresar" />
-
-            <p>¿No tienes una cuenta?</p>&nbsp;
-          <a href="" className="a-links"> <p>¡Haz click aquí!</p> </a> 
-
-            <button type="button" class="button">
-		<span class="button__text">Continuar con Google&nbsp;&nbsp;&nbsp;&nbsp;</span>
-		<span class="button__icon">
-    <ion-icon name="logo-google"></ion-icon>
-		</span>
-	</button>
-
-            <button type="button" class="button">
-		<span class="button__text">Continuar con Facebook</span>
-		<span class="button__icon">
-    <ion-icon name="logo-facebook"></ion-icon>
-		</span>
-	</button>
-        </form>
-    );
+  return (
+    <form className="register-register" onSubmit={login}>
+      <h2 className="register-title">Iniciar sesión</h2>
+      <label className="labels-register" htmlFor="email">
+        Nombre de usuario
+      </label>
+      <input
+        type="text"
+        id="email"
+        className="input-register"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <label className="labels-register" htmlFor="password">
+        Contraseña
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </label>
+      <input
+        type="password"
+        id="password"
+        className="input-register"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input type="submit" className="register-btn" value="Ingresar" />
+      <p>¿No tienes una cuenta?</p>&nbsp;
+      <a href="" className="a-links">
+        {" "}
+        <p>¡Haz click aquí!</p>{" "}
+      </a>
+    </form>
+  );
 }
 
 export default LoginForm;
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -3,11 +3,16 @@ import Footer from "../components/Footer";
 import LoginForm from "../components/LoginForm";
 import RegisterForm from "../components/RegisterForm";
 import "../assets/stylesheets/form-dynamic.css"
+import { useEffect, useState } from "react";
+import { authenticate, isAuthenticated } from "../resources/Auth";
+import { useNavigate } from "react-router-dom";
 
 
 
 function SignUp() {
 
+  //Funciones handler para hacer la transición
+  // De los paneles
   function signHandler(){
     container.classList.add("right-panel-active");
    }
@@ -15,11 +20,53 @@ function SignUp() {
    function signHandler2(){
     container.classList.remove("right-panel-active");
    }
-   
 
+   ////////////LÓGICA DEL INICIO DE SESIÓN////////////
+   const navigate = useNavigate();
+   const [email, setEmail] = useState(null);
+   const [password, setPassword] = useState(null);
 
+   function login(e){
+       e.preventDefault()
+       authenticate(email,password).then(
+           (data) => {
+               if (!data.logged) {
+                 alert("Verifique sus datos")
+               } else {
+                 navigate("/")
+               }
+           }
+       )
+   }
 
-   
+   useEffect(() => { if(isAuthenticated()){
+       alert("Ya tienes una sesión iniciada")
+       navigate("/")
+     } }, [])
+   ///////////////////////////////////////////////////
+   //////////////LÓGICA PARA EL REGISTRO /////////////////
+   const [usernameRegister, setUsernameRegister] = useState("");
+   const [emailRegister, setEmailRegister] = useState("");
+   const [passwordRegister, setPasswordRegister] = useState("");
+ 
+   function sendRegister(e) {
+     e.preventDefault()
+     register(usernameRegister, emailRegister, passwordRegister).then(
+       (data) => {
+         if (!data.logged) {
+           alert("Este nombre de usuario ya está en uso")
+         } else {
+           navigate("/")
+         }
+       }
+     )
+   }
+ 
+   useEffect(() => { if(isAuthenticated()){
+     alert("Ya tienes una sesión iniciada")
+     navigate("/")
+   } }, [])
+    ///////////////////////////////////////////////////
   return (
     <>
       <Header />
@@ -34,25 +81,39 @@ function SignUp() {
 
 
 	<div className="form-container sign-up-container">
-		<form action="#" className="form-dynamic">
-			<h1>Create Account</h1>
+
+      {/* FORMULARIO DEL REGISTRO DE CUENTA */}
+
+		<form onSubmit={sendRegister} className="form-dynamic">
+			<h1>Crea tu cuenta</h1>
 			<div className="social-container">
 				<a href="#" className="social"><ion-icon name="logo-google"></ion-icon></a>
 				<a href="#" className="social"><ion-icon name="logo-facebook"></ion-icon></a>
 				<a href="#" className="social"><ion-icon name="logo-twitter"></ion-icon></a>
 			</div>
-			<span>or use your email for registration</span>
-			<input type="text" placeholder="Name" />
-			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Password" />
-			<button className="btn-sign-up">Sign Up</button>
+			<span>O usa tu correo para registrarte</span>
+			<input type="text" placeholder="Usuario"  onChange={(e) => {
+          setUsernameRegister(e.target.value);
+        }}/>
+			<input type="email" placeholder="Correo electrónico" onChange={(e) => {
+          setEmailRegister(e.target.value);
+        }}/>
+			<input type="password" placeholder="Contraseña"  onChange={(e) => setPasswordRegister(e.target.value)}/>
+			<button className="btn-sign-up">Registrarse</button>
 		</form>
+
+
+
 	</div>
 
 
 
 	<div className="form-container sign-in-container">
-		<form action="#">
+
+
+    {/* FORMULARIO DEL INICIO DE SESIÓN */}
+
+		<form onSubmit={login}>
 			<h1>Iniciar Sesión</h1>
 			<div class="social-container">
 				<a href="#" className="social"><ion-icon name="logo-google"></ion-icon></a>
@@ -60,11 +121,18 @@ function SignUp() {
 				<a href="#" className="social"><ion-icon name="logo-twitter"></ion-icon></a>
 			</div>
 			<span>O usa tu cuenta</span>
-			<input type="text" placeholder="Nombre de usuario" />
-			<input type="password" placeholder="Contraseña" />
+			<input type="text" placeholder="Nombre de usuario" onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        />
+			<input type="password" placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)} />
 			<a href="#">¿Olvidaste tú contraseña?</a>
 			<button className="btn-sign-up">Ingresar</button>
 		</form>
+
+
+
+
 	</div>
 
 
@@ -77,17 +145,17 @@ function SignUp() {
 
 
 			<div className="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal details</p>
-				<button className="ghost" id="signIn" onClick={signHandler2}>Sign In</button>
+				<h1>¡Bienvenido de vuelta compañero!</h1>
+				<p>Para seguir conectado con nosotros y poder hacer un pedido haz click al botón de abajo para iniciar sesión</p>
+				<button className="ghost" id="signIn" onClick={signHandler2}>Ingresar</button>
 			</div>
 
 
 
 			<div className="overlay-panel overlay-right">
-				<h1>Hi There!</h1>
-				<p>Enter your personal details to open an account with us</p>
-				<button className="ghost btn-sign-up" id="signUp" onClick={signHandler}>Sign Up</button>
+				<h1>¡Bienvenido!</h1>
+				<p>Haz click en el botón de abajo para abrir una cuenta con nosotros</p>
+				<button className="ghost btn-sign-up" id="signUp" onClick={signHandler}>Crear cuenta</button>
 			</div>
 
 
